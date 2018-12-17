7d80eaff1dc06769d7404744bba7744e9b6a1de1
Przemyslaw Wieteska <wieteska.przemyslaw@gmail.com>
2018-12-17 14:18:26 +0100

    up

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+const path = require("path");
 
 const items = require("./routes/api/items");
 
@@ -17,6 +18,14 @@ mongoose
   )
   .then(() => console.log("MongoDB connected!"))
   .catch(err => console.log(err));
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 app.use("/api/items", items);
 
